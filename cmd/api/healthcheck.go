dd2724a1e1dfeaf6668a01ba35c2149a83ad7977
QuentinZoli <quentin_liso@live.fr>
2025-11-03 17:03:15 +0100

    3.6 Sending error messages

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -16,8 +16,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 
 }
