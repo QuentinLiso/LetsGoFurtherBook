cf58b7099e3b1e698fc541998383505651176874
QuentinZoli <quentin_liso@live.fr>
2025-11-04 22:29:51 +0100

    7.1 Setting up the movie model

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"greenlight.qliso.net/internal/validator"
@@ -33,3 +34,22 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
     v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
 
+type MovieModel struct {
+	DB	*sql.DB
+}
+
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+func (m MovieModel) Delete(id int64) error {
+	return nil
+}
