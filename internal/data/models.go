e0774fac03b851cab92ba10f1dd0f78576b1a790
QuentinZoli <quentin_liso@live.fr>
2025-11-08 21:18:33 +0100

    12.3 Registering a user

--- internal/data/models.go
+++ internal/data/models.go
@@ -11,11 +11,13 @@ var (
 )
 
 type Models struct {
-	Movies MovieModel
+	Movies	MovieModel
+	Users	UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies:	MovieModel{DB: db},
+		Users: UserModel{DB: db},
 	}
 }
