c89b88d3fcac0141e5127b57c5cfa55ce68bbfe9
QuentinZoli <quentin_liso@live.fr>
2025-11-08 23:53:23 +0100

    13.3 Sending a welcome email

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"greenlight.qliso.net/internal/data"
+	"greenlight.qliso.net/internal/mailer"
 
 	_ "github.com/lib/pq"
 )
@@ -29,12 +30,20 @@ type config struct {
 		burst	int
 		enabled	bool
 	}
+	smtp struct {
+		host		string
+		port		int
+		username	string
+		password	string
+		sender		string
+	}
 }
 
 type application struct {
 	config	config
 	logger	*slog.Logger
 	models	data.Models
+	mailer	*mailer.Mailer
 }
 
 func main() {
@@ -42,13 +51,21 @@ func main() {
 	
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostreSQL DSN")
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostreSQL max idle connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15 * time.Minute, "PostreSQL max connection idle time")
+	
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "b7d201885df149", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "9c07643b812bf5", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.qliso.net>", "SMTP sender")
 
 	flag.Parse()
 
@@ -64,10 +81,17 @@ func main() {
 
 	logger.Info("database connection pool established")
 
+	mailer, err := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models:	data.NewModels(db),
+		mailer: mailer,
 	}
 
 	err = app.serve()
