29107a0257f26e465e2aaecf3b0282300a2bd0cb
QuentinZoli <quentin_liso@live.fr>
2025-11-11 22:18:14 +0100

    17.3 Simple CORS requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -159,3 +159,19 @@ func (app *application) requirePermissions(code string, next http.HandlerFunc) h
 	return app.requireActivatedUser(fn)
 }
 
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Origin")
+		origin := r.Header.Get("Origin")
+
+		if origin != "" {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
+}
