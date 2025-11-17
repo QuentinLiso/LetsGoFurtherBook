05957245511dde797f93fb5f8872bc092f5ce4de
QuentinZoli <quentin_liso@live.fr>
2025-11-17 20:27:33 +0100

    Generate version number automatically

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"expvar"
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"runtime"
@@ -14,11 +15,14 @@ import (
 
 	"greenlight.qliso.net/internal/data"
 	"greenlight.qliso.net/internal/mailer"
+	"greenlight.qliso.net/internal/vcs"
 
 	_ "github.com/lib/pq"
 )
 
-const version = "1.0.0"
+var (
+	version = vcs.Version()
+)
 
 type config struct {
 	port int
@@ -80,7 +84,14 @@ func main() {
 		return nil
 	})
 
+	displayVersion := flag.Bool("version", false, "Display version and exit")
+
 	flag.Parse()
+
+	if *displayVersion {
+		fmt.Printf("Version:\t%s\n", version)
+		os.Exit(0)
+	}
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
